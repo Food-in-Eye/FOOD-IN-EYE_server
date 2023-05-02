aa352dec00f50c2a923a45face9826a7c9129e59
JIIN <j00j12i11@duksung.ac.kr>
2023-05-02 14:24:37 +0900

    add v3_router copy from v2_router & v1_router is now unavailable

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from v1.api import v1_router
 from v2.api import v2_router
+from v3.api import v3_router
 
 app = FastAPI()
 
-app.include_router(v1_router)
 app.include_router(v2_router)
+app.include_router(v3_router)
 
 @app.get("/")
 async def hello():
